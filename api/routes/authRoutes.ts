71abc3388b97f1e2c18ff68746536e52ef5163ca
Christian Lee-Flinders <cleeflinders@gmail.com>
2025-10-28 21:06:40 +0000

    feat: update Prisma schema and seed data for enhanced user management

--- api/routes/authRoutes.ts
+++ api/routes/authRoutes.ts
@@ -1,13 +1,9 @@
 import { Router } from "express";
 import { login, register } from "../controllers/authControllers";
-import { authenticateJWT, autherisedRoles } from "../middleware/auithMiddleware";
 
 const router = Router();
 
 router.post('/register', register)
 router.post('/login', login)
-router.get('/test', authenticateJWT ,(_req, res) => {
-    res.json({ message: 'Auth route is working' });
-});
 
 export default router;
